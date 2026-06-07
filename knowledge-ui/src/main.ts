bc7f6af5e31a3037d9c8ba28afe9820c5ae1a8de
visterion <46775082+visterion@users.noreply.github.com>
2026-06-07 14:50:58 +0200

    feat(ui): add Sovereign Knowledge design tokens + global base

--- knowledge-ui/src/main.ts
+++ knowledge-ui/src/main.ts
@@ -4,6 +4,7 @@ import { vuetify } from './plugins/vuetify'
 import { router } from './router'
 import { i18n } from './i18n'
 import App from './App.vue'
+import './styles/tokens.css'
 import './style.css'
 
 createApp(App).use(createPinia()).use(vuetify).use(router).use(i18n).mount('#app')
